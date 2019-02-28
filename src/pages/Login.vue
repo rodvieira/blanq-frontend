0d57701fe139f5b56c9d3fbee63a676f95aefa61
rdvieira <rodrigodeniz@gmail.com>
2019-02-27 23:36:44 -0300

    First Build Heroku

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -11,11 +11,6 @@
 </template>
 
 <script>
-// baseURL: 'https://api.moskitcrm.com/v1/',
-//     headers: {
-//         'apikey':'70bac45c-cef6-4c36-bb33-7958a200c96a'
-//     }
-
 import { mapState, mapActions } from "vuex";
 
 export default {
